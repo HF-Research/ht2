6ac49bc0807e11660a8d617e27ef8a50a9908a52
Matthew Phelps <ma.phelps@gmail.com>
2019-10-11 14:25:45 +0200

    Test trasnfer to HF-Research

--- ui.r
+++ ui.r
@@ -12,5 +12,6 @@ ui <- fluidPage(
     collapsible = TRUE,
     source(file.path("ui", "main_ui.R"), local = TRUE)$value,
     source(file.path("ui", "about_ui.R"), local = TRUE)$value
+    
   )
 )
