204ef0570d47dfd45a7e86e05238d50797dfe278
Matthew Phelps <ma.phelps@gmail.com>
2019-10-14 11:07:42 +0200

    Added HF logo to banner ontop of navbar. This is more like what HF main pages is like. Will need to be modified to suit webdesign standards for HF

--- ui.r
+++ ui.r
@@ -1,14 +1,18 @@
-ui <- fluidPage(
-  div(style = "padding-left: -15px; padd-right: -15px;",
-      titlePanel(
-        title = "", windowTitle = "HjerteTal"
-      )),
+ui <- div(
+  img(
+    src = "hf-logo.png",
+    align = "left",
+    style = "padding-top: 20px; padding-bottom: 40px; padding-left: 2.5rem;",
+    height = "110px"
+  ),
+  fluidPage(
+    div(style = "padding-left: 0px; padding-right: 0px;",
+        titlePanel(
+          title = "", windowTitle = "HjerteTal"
+        ))
+  ),
   navbarPage(
-    title = div(img(
-      src = "hf-logo.png",
-      style = "margin-top: -10px;",
-      height = 40
-    )),
+    title = "HjerteTal",
     collapsible = TRUE,
     source(file.path("ui", "main_ui.R"), local = TRUE)$value,
     source(file.path("ui", "about_ui.R"), local = TRUE)$value
