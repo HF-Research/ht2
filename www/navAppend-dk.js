4021b971c62225ed1b1091166edabc1d988301b3
Marianna Meaidi <mmeaidi@hjerteforeningen.dk>
2025-03-10 11:03:45 +0100

    remove English version from new HJerteTAl

--- www/navAppend-dk.js
+++ www/navAppend-dk.js
@@ -1,4 +1,4 @@
 /*JS code to add link to en/dk version of website*/
-$(document).ready(function() {
+/*$(document).ready(function() {
   $(".navbar .container-fluid .navbar-nav ").append('<li><a href="https://hjerteforeningen.shinyapps.io/HjerteTal-en/" target="_blank">English version</a></li>');
-});
+});*/
